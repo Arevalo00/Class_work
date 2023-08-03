b03280833291f0d943dbcfbbdd998cb90b6bf7ec
jennifer arevalo <arevalo5007@gmail.com>
2023-08-02 22:52:27 -0500

    add

--- week_12/16-Stu_Schema/Unsolved/db/schema.sql
+++ week_12/16-Stu_Schema/Unsolved/db/schema.sql
@@ -1,1 +1,22 @@
 -- Write your schema here --
+
+DROP DATABASE IF EXISTS shop_db;
+CREATE DATABASE shop_db; 
+
+USE shop_db; 
+
+CREATE TABLE prouducts (
+    id INT NOT NULL,
+    prouduct_name VARCHAR (30) NOT NULL, 
+    category_name VARCHAR (100) NOT NULL, 
+    price INT NOT NULL,
+    in_stock BOOLEAN NOT NULL
+);
+
+CREATE TABLE categorys (
+
+    id INT NOT NULL,
+    category_name VARCHAR (30) NOT NULL
+);
+
+
--- week_12/20-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
+++ week_12/20-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
@@ -1,1 +1,22 @@
 -- Write your Schema Here -- 
+DROP DATABASE IF EXISTS shop_db;
+CREATE DATABASE shop_db;
+
+USE shop_db;
+
+CREATE TABLE customers (
+    id INT NOT NULL ,
+    frist_name VARCHAR (30) NOT NULL, 
+    last_name VARCHAR (30) NOT NULL, 
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE customers_order(
+    id INT,
+    customers_id INT, 
+    order_details TEXT, 
+    FOREIGN KEY (customers_id)
+    REFERENCES customers(id)
+    ON DELETE SET NULL 
+); 
+
